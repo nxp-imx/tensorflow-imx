27ba6cb8337da8d15c78526b26f8bf8ce62ba241
Kainan Cha <kainan.cha@verisilicon.com>
2020-06-24 06:36:46 +0200

    Relax rule for ResizeBilinear operation
    Relax ResizeBilinear operation requirement for
    align_corners and half_pixel_centers for NNAPI delegate
    on Linux.
    
    Change-Id: I21f5f4d18a7a7105aa33b3fa51cf52f309782f4c
    Signed-off-by: Kainan Cha <kainan.cha@verisilicon.com>
    (cherry picked from commit 8616ee2099cd389de2532a2892a1ef776bc8dbd1)

--- tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
+++ tensorflow/lite/delegates/nnapi/nnapi_delegate.cc
@@ -1968,6 +1968,7 @@ bool NNAPIDelegateKernel::Validate(
       }
       auto builtin =
           reinterpret_cast<TfLiteResizeBilinearParams*>(node->builtin_data);
+      #if defined __ANDROID__    // Relax Rule for ResizeBilinear Operator
       if (android_sdk_version <= kMinSdkVersionForNNAPI12) {
         Expect(!builtin->align_corners,
                NNAPIValidationFailureType::kUnsupportedOperandValue,
@@ -1976,6 +1977,7 @@ bool NNAPIDelegateKernel::Validate(
                NNAPIValidationFailureType::kUnsupportedOperandValue,
                "NNAPI does not support half_pixel_centers == true.", &val_ctx);
       }
+      #endif
       if (android_sdk_version < kMinSdkVersionForNNAPI12) {
         Expect(input.type == kTfLiteFloat32,
                NNAPIValidationFailureType::kUnsupportedInputType,
@@ -2435,7 +2437,7 @@ bool NNAPIDelegateKernel::Validate(
              "NNAPI does not support generating a scalar as output for MEAN.",
              &val_ctx);
 
-    #if defined __ANDROID__
+    #if defined __ANDROID__  //Relax Rule for Mean Operator
       auto input_param = context->tensors[node->inputs->data[0]].params;
       auto output_param = context->tensors[node->outputs->data[0]].params;
       Expect(input_param.scale == output_param.scale &&
