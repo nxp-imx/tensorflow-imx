741f4d24be0786c1a087fa053893098eb11c1a3e
Feng Guo <feng.guo@nxp.com>
2024-12-18 23:15:54 +0800

    Fix compile issue when enable test
    Signed-off-by: Feng Guo <feng.guo@nxp.com>

--- tensorflow/lite/tools/cmake/modules/Findgoogletest.cmake
+++ tensorflow/lite/tools/cmake/modules/Findgoogletest.cmake
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if(TARGET googletest OR googletest_POPULATED)
+if(TARGET gtest OR googletest_POPULATED)
   return()
 endif()
 
