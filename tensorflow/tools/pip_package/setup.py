10fddafb825dbeec4efacb4669cf8f21e7e67e5e
Feng Guo <feng.guo@nxp.com>
2025-07-02 11:12:45 +0800

    The shared library build correspond to the build rules in bazel for shared library.
    Signed-off-by: Feng Guo <feng.guo@nxp.com>

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -47,7 +47,8 @@ from setuptools.dist import Distribution
 # For pip, we will remove all '-' characters from this string, and use the
 # result for pip.
 # Also update tensorflow/tensorflow.bzl and
-# tensorflow/core/public/version.h
+# tensorflow/core/public/version.h and
+# tensorflow/lite/CMakeLists.txt
 _VERSION = '2.19.0'
 
 # Update this version when a new libtpu stable version is released.
